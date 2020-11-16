41ba0b52b0dad6a96a2685b4c86c1a237e5ad0e1
OHHYEONJEONG <guswjd3927@naver.com>
2020-11-16 16:13:17 +0900

    test

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="/">Happy House</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/apt">아파트정보</b-nav-item>
+          <b-nav-item href="/qnaboard">QnA 게시판</b-nav-item>
+          <b-nav-item href="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
---
+++ src/views/Board.vue
@@ -0,0 +1,12 @@
+<template>
+        <h2>게시판 글 목록</h2>
+</template>
+
+<script>
+
+export default {
+  name: 'Board',
+};
+</script>
+
+<style></style>
