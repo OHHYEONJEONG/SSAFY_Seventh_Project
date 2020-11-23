710dc49cd2a6d01243a1d1e84cc4e28f3d29407f
upswp <tkddnsos@gmail.com>
2020-11-23 19:30:09 +0900

    QnA게시판 업데이트 게시판 생성

--- src/components/UpdateBoard.vue
+++ src/components/UpdateBoard.vue
@@ -2,60 +2,63 @@
   <div>
     <h3>사원정보</h3>
     <br />
-    <router-link class="btn btn-primary" to="/qnaboard"
-      >모든 글 보기</router-link
+    <v-btn
+      id="subtitle"
+      v-for="(sub, index) in subnav"
+      :key="index"
+      :to="sub.path"
+      text
     >
-    |
-    <router-link class="btn btn-primary" to="/insert">글 등록하기 </router-link>
+      {{ sub.title }}
+    </v-btn>
     <br /><br />
 
-    <v-simple-table
-      style="width:500px; margin-left:auto; margin-right:auto;border:1px solid black; border-collapse:collase;"
-      dense
-    >
-      <tr>
-        <td>글번호</td>
-        <td v-html="article.no"></td>
-      </tr>
-      <tr>
-        <td>작성자</td>
-        <td v-html="article.writer"></td>
-      </tr>
-      <tr>
-        <th>제목</th>
-        <td>
-          <input
-            data-msg="제목"
-            type="text"
-            name="title"
-            id="_title"
-            size="20"
-            v-model="article.title"
-          />
-        </td>
-      </tr>
-      <tr>
-        <th>내용</th>
-        <td>
-          <input
-            data-msg="내용"
-            type="text"
-            name="content"
-            id="_content"
-            size="30"
-            v-model="article.content"
-            style="width:100%; height:100px;"
-          />
-        </td>
-      </tr>
-      <tr>
-        <td colspan="2" style="height:50px; text-align:center;">
-          <v-btn type="submit" name="button" @click="updateBoard">
-            글수정
-          </v-btn>
-        </td>
-      </tr>
-    </v-simple-table>
+    <v-card class="mt-3" id="card">
+      <v-simple-table>
+        <template v-slot:default>
+          <tbody>
+            <tr>
+              <td>글번호</td>
+              <td v-html="article.no"></td>
+            </tr>
+            <tr>
+              <td>작성자</td>
+              <td v-html="article.writer"></td>
+            </tr>
+            <tr>
+              <td>제목</td>
+              <td>
+                <v-text-field
+                  data-msg="제목"
+                  type="text"
+                  name="title"
+                  id="_title"
+                  size="20"
+                  v-model="article.title"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>내용</td>
+              <td>
+                <v-text-field
+                  data-msg="내용"
+                  type="text"
+                  name="content"
+                  size="30"
+                  v-model="article.content"
+                  style="width:100%; height:100px;"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+    <br />
+    <v-btn type="submit" name="button" @click="updateBoard">
+      글수정
+    </v-btn>
   </div>
 </template>
 
@@ -68,6 +71,11 @@ export default {
     return {
       upHere: false,
       article: {},
+      subnav: [
+        { title: '모든 글 보기', path: '/qnaboard' },
+        { title: '글 등록하기 ', path: '/insert' },
+      ],
+
       loading: true,
       errored: false,
     };
@@ -114,18 +122,13 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#text {
+  text-align: left;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#card {
+  position: relative;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#subtitle {
+  float: right;
 }
 </style>
