8df6e02618e3d3897a5e3992ae3fef3f8505551d
upswp <tkddnsos@gmail.com>
2020-11-24 20:41:25 +0900

    공지사항 수정 관리자/일반사용자 추가

--- src/components/Notice/UpdateNotice.vue
+++ src/components/Notice/UpdateNotice.vue
@@ -2,15 +2,22 @@
   <div>
     <h3>공지사항 수정</h3>
     <br />
-    <v-btn
-      id="subtitle"
-      v-for="(sub, index) in subnav"
-      :key="index"
-      :to="sub.path"
-      text
-    >
-      {{ sub.title }}
-    </v-btn>
+    <!-- 관리자일때 등록, 수정 가능하도록  -->
+    <template v-if="user.userno == 1">
+      <v-btn
+        id="subtitle"
+        v-for="(sub, index) in subnav"
+        :key="index"
+        :to="sub.path"
+        text
+      >
+        {{ sub.title }}
+      </v-btn>
+    </template>
+    <!-- 관리자가 아닐경우 조회만 가능하도록  -->
+    <template v-else>
+      <v-btn id="subtitle" :to="elsenav.path">{{ elsenav.title }}</v-btn>
+    </template>
     <br /><br />
 
     <v-card class="mt-3" id="card">
@@ -56,26 +63,33 @@
       </v-simple-table>
     </v-card>
     <br />
-    <v-btn type="submit" name="button" @click="updateBoard">
-      글수정
-    </v-btn>
+    <template v-if="user.userno == 1">
+      <v-btn type="submit" name="button" @click="updateBoard">
+        글수정
+      </v-btn>
+    </template>
   </div>
 </template>
 
 <script>
 import http from '../../http-common';
+import axios from 'axios';
+const SERVER_URL = process.env.VUE_APP_SERVER_URL;
+
 export default {
-  name: 'SelectBoardByNo',
+  name: 'UpdateNotice',
   props: ['no'],
   data() {
     return {
       upHere: false,
       article: {},
       subnav: [
-        { title: '모든 글 보기', path: '/noticeboard' },
-        { title: '글 등록하기 ', path: '/insert' },
+        { title: '모든 글 보기', path: '/notice' },
+        { title: '글 등록하기 ', path: '/insertNotice' },
       ],
+      elsenav: { title: '공지사항 전체보기', path: '/notice' },
 
+      user: '',
       loading: true,
       errored: false,
     };
@@ -90,11 +104,11 @@ export default {
         alert('내용을 작성하세요.');
         return;
       } else {
-        this.$router.push('/noticeboard/');
+        this.$router.push('/notice/');
       }
 
       http
-        .put('/noticeboard/qupdate/' + this.article.no, this.article)
+        .put('/noticeboard/update/' + this.article.no, this.article)
         .then((response) => {
           if (response.data == 'success') {
             alert('글등록 업데이트처리를 하였습니다.');
@@ -113,12 +127,24 @@ export default {
   },
   mounted() {
     http
-      .get('/noticeboard/qdetail/' + this.no)
+      .get('/noticeboard/detail/' + this.no)
       .then((response) => (this.article = response.data))
       .catch(() => {
         this.errored = true;
       })
       .finally(() => (this.loading = false));
+  },
+  created() {
+    // 가져온 Token값을 header에 넣어주는 작업 실시.
+    axios.defaults.headers.common['auth-token'] = this.$store.state.accessToken;
+    axios
+      .get(`${SERVER_URL}/user/info`)
+      .then((response) => {
+        this.user = response.data.user;
+      })
+      .catch(() => {
+        // this.$store.dispatch('LOGOUT').then(() => this.$router.replace('/'));
+      });
   },
 };
 </script>
